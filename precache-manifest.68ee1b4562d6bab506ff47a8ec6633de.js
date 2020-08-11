9cf00e638a190b299232be7aa311e117ae2b744c
Adnan Birparic <adnan.birparic@gmail.com>
2020-08-11 03:17:57 +0200

    Updates

---
+++ precache-manifest.68ee1b4562d6bab506ff47a8ec6633de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb0d1d7268242f79bc5d76fe1576148d",
+    "url": "/react_puzzle/index.html"
+  },
+  {
+    "revision": "3e319258dc12396e64f9",
+    "url": "/react_puzzle/static/css/main.b9fcea51.chunk.css"
+  },
+  {
+    "revision": "2bc2a83a667ff7a31f6d",
+    "url": "/react_puzzle/static/js/2.ae336ef7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_puzzle/static/js/2.ae336ef7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e319258dc12396e64f9",
+    "url": "/react_puzzle/static/js/main.1aa1e406.chunk.js"
+  },
+  {
+    "revision": "658ef6471b2cd5891100",
+    "url": "/react_puzzle/static/js/runtime-main.b4c2ee8a.js"
+  }
+]);
